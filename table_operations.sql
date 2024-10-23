6674c30a6473dfe29d16e3b78b4d68c2f78f0391
Mert Kav <67902681+mertkav@users.noreply.github.com>
2024-10-23 15:29:28 +0300

    Update table_operations.sql

--- table_operations.sql
+++ table_operations.sql
@@ -1,12 +1,12 @@
   -- Brief Introduction: When I imported the CSV file into BigQuery, I noticed that the anonymized_user_id values were imported as floats. My goal was to convert this float column into a string, so I performed table operations, applied data pre-processing, and improved the readability of the data.
-  -- 1. Adding a new column `user_id` of type string and updating it with a formatted version of `anonymized_user_id`.
+   -- 1. Adding a new column `user_id` of type string.
 ALTER TABLE
-  dataset.data_table ADD COLUMN user_id string;
-  -- 2. Populating the newly added `user_id` column with the formatted version of `anonymized_user_id`, ensuring non-null user IDs are carried over.
+  dataset.data_table ADD COLUMN user_id STRING;
+  -- 2. Populating the newly added `user_id` column by casting `anonymized_user_id` to a string and removing the decimal point.
 UPDATE
   dataset.data_table
 SET
-  user_id = FORMAT('%f', anonymized_user_id)
+  user_id = SAFE_CAST(CAST(anonymized_user_id AS INT64) AS STRING)
 WHERE
   anonymized_user_id IS NOT NULL;
   -- 3. Dropping the `anonymized_user_id` column, as it is no longer needed since we've transferred its data to `user_id`.
@@ -35,10 +35,10 @@ DROP TABLE
   dataset.data_table;
   -- 7. Renaming the newly created table (`data_table_arranged`) back to the original table name.
 ALTER TABLE
-  `dataset.data_table_arranged`
+  dataset.data_table_arranged
 RENAME
   TO data_table;
-  -- 8. Checking as we want 
+  -- 8. Verifying the structure.
 SELECT
   *
 FROM
