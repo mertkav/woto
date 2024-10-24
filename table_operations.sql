34d7829babb3b139077dcf80d06ea4802a6f2794
Mert Kav <67902681+mertkav@users.noreply.github.com>
2024-10-24 21:41:08 +0300

    Update table_operations.sql

--- table_operations.sql
+++ table_operations.sql
@@ -1,25 +1,24 @@
   -- Brief Introduction: When I imported the CSV file into BigQuery, I noticed that the anonymized_user_id values were imported as floats. My goal was to convert this float column into a string, so I performed table operations, applied data pre-processing, and improved the readability of the data.
-   -- 1. Adding a new column `user_id` of type string.
+   -- Adding a new column `user_id` of type string.
 ALTER TABLE
   dataset.data_table ADD COLUMN user_id STRING;
-  -- 2. Populating the newly added `user_id` column by casting `anonymized_user_id` to a string and removing the decimal point.
 UPDATE
   dataset.data_table
 SET
   user_id = SAFE_CAST(CAST(anonymized_user_id AS INT64) AS STRING)
 WHERE
   anonymized_user_id IS NOT NULL;
-  -- 3. Dropping the `anonymized_user_id` column, as it is no longer needed since we've transferred its data to `user_id`.
+  -- Dropping the `anonymized_user_id` column
 ALTER TABLE
   dataset.data_table
 DROP
   COLUMN anonymized_user_id;
-  -- 4. Selecting all the data from `dataset.data_table` to review the current structure of the table.
+  -- Selecting all the data to check them all
 SELECT
   *
 FROM
   `dataset.data_table`;
-  -- 5. Creating a new table `dataset.data_table_arranged` with the desired column order. Here, we are moving `user_id` to the first column and ordering other columns.
+  --Creating a new table `dataset.data_table_arranged` with the desired column order
 CREATE TABLE
   dataset.data_table_arranged AS
 SELECT
@@ -31,15 +30,15 @@ SELECT
   source_keyword
 FROM
   dataset.data_table;
-  -- 6. Dropping the original table to replace it with the newly arranged version.
+  -- Dropping the original table to replace it with the newly arranged version.
 DROP TABLE
   dataset.data_table;
-  -- 7. Renaming the newly created table (`data_table_arranged`) back to the original table name.
+  --Renaming the newly created table back to the original table name.
 ALTER TABLE
   dataset.data_table_arranged
 RENAME
   TO data_table;
-  -- 8. Verifying the structure.
+  -- Checking it 
 SELECT
   *
 FROM
